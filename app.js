cd534ac09066d9f9d2e1ba2e30fa0f3240c09cbc
g g <gabedotgutierrez@gmail.com>
2024-09-04 19:04:46 -0400

    using event.target, got multplication to work, definitely spaghetti code

---
+++ app.js
@@ -0,0 +1,55 @@
+/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('.display')
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+let nums = []
+let operator = ""
+let solution =  ""
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+// buttons.forEach((button) => {
+//     button.addEventListener('click', (event) => {
+//       // This log is for testing purposes to verify we're getting the correct value
+//       console.log(event.target.innerText);
+//       // Future logic to capture the button's value would go here...
+//     });
+//   });
+
+  calculator.addEventListener('click', (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    // You have to click a button to see this log
+    console.log(event.target.innerText);
+  
+    // Example
+    if (event.target.classList.contains('number')) {
+      // Do something with a number
+      display.innerText = event.target.innerText
+      nums.push(event.target.innerText)
+      console.log(nums) 
+    }
+  
+    // Example
+    if (event.target.innerText === '*') {
+      // Do something with this operator
+      operator = "*"
+      display.innertext = ""
+    //   console.log(operator)
+    } 
+    if (event.target.innerText === '=') {
+        if (operator === '*'){
+            solution = nums[0] * nums [1]
+            display.innerText = solution
+        }
+        nums = 
+        nums.push(solution)
+    }
+
+
+
+  });
+/*-------------------------------- Functions --------------------------------*/
